beb7cf41d032f2e5b2d3043b0a613cc0459417d0
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-25 16:22:17 +0530

    added home page

---
+++ src/Jweather.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './Pages/Home/Home';
+
+
+const Jweather:React.FC = ()=>{
+       return (
+              <Home/>
+       );
+}
+
+export default Jweather;
