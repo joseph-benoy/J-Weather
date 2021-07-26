a04dcb1304bbbf2dd2786385f2017ed51b1f965b
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-26 12:13:25 +0530

    added drawer list

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -169,21 +169,22 @@ const Header = ()=> {
         </div>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            <ListItem button>
+              <ListItemIcon><HomeOutlinedIcon/></ListItemIcon>
+              <ListItemText primary="Home" />
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            <ListItem button>
+              <ListItemIcon><FilterDramaOutlinedIcon/></ListItemIcon>
+              <ListItemText primary="Weather" />
             </ListItem>
-          ))}
+            <ListItem button>
+              <ListItemIcon><ContactSupportOutlinedIcon/></ListItemIcon>
+              <ListItemText primary="About" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><LocalPhoneOutlinedIcon/></ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItem>
         </List>
       </Drawer>
     </div>
