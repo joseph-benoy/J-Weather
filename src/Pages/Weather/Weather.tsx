29f37e84acaf40774268b624a7914c66dac67d3f
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-27 12:59:45 +0530

    changes form layout

--- src/Pages/Weather/Weather.tsx
+++ src/Pages/Weather/Weather.tsx
@@ -4,6 +4,9 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { TextField, Typography } from '@material-ui/core';
 import MainButton from '../../Components/MainButton/MainButton';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import { useSelector,useDispatch } from 'react-redux';
+import {RootState} from '../../Redux/Store';
+import { useForm,SubmitHandler } from 'react-hook-form';
 
 const useStyles = makeStyles((theme:Theme)=>createStyles({
        root:{
@@ -39,6 +42,9 @@ const useStyles = makeStyles((theme:Theme)=>createStyles({
 
 const Weather:React.FC = ()=>{
        const classes = useStyles();
+       const dispatch = useDispatch();
+       const queryCity = useSelector((state:RootState)=>state.weather.city);
+       console.log(queryCity);
        return(
               <Grid container className={classes.root}>
                      <Grid item xs={12}>
@@ -46,11 +52,17 @@ const Weather:React.FC = ()=>{
                                    Let's know about your city
                             </Typography>
                      </Grid>
-                     <Grid item xs={12} lg={10}>
-                            <TextField fullWidth  variant="outlined" type="text" placeholder="eg. Mumbai" name="city"/>
-                     </Grid>
-                     <Grid item xs={12} container lg={2} className={classes.buttonGrid} alignItems="stretch">
-                            <MainButton text="search" icon={<SearchOutlinedIcon/>}/>
+                     <Grid item xs={12}>
+                            <form>
+                                   <Grid container item xs={12} spacing={2}>
+                                   <Grid item xs={12} lg={10}>
+                                          <TextField fullWidth  variant="outlined" type="text" placeholder="eg. Mumbai" name="city"/>
+                                   </Grid>
+                                   <Grid item xs={12} container lg={2} className={classes.buttonGrid} alignItems="stretch">
+                                          <MainButton type="submit" text="search" icon={<SearchOutlinedIcon/>}/>
+                                   </Grid>
+                                   </Grid>
+                            </form>
                      </Grid>
                      <Grid item xs={12} container justifyContent="center">
                             <img src="./images/city.svg" alt="city" className={classes.coverImage}/>
--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import weatherReducer from './Weather/WeatherSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    weather:weatherReducer
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
