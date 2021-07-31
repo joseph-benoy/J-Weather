276bec535187351c90adc456075ccce18b72f6ea
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-31 19:17:32 +0530

    add getweatherbycity to weather controller

--- server/weather/controllers/weather.controller.ts
+++ server/weather/controllers/weather.controller.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import debug from 'debug';
+import weatherService from '../services/weather.service';
 
 const log:debug.IDebugger = debug('app:weather-controller');
 
 class WeatherController{
        async getWeatherByCity(req:express.Request,res:express.Response,next:express.NextFunction){
-              
+              const weatherData = await weatherService.getWeatherByCity(req.query.city as string);
+              res.status(200).json(weatherData);
        }
 }
+
+export default new WeatherController();
