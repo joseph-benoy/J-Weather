e5bd1ad5ef99259447574ea792ef70d9a492d865
Joseph Benoy <josephbenoy03@gmail.com>
2021-08-01 00:53:42 +0530

    added message dao and updated

--- server/message/daos/message.dao.ts
+++ server/message/daos/message.dao.ts
@@ -1,1 +1,45 @@
 import mongooseService from "../../common/services/mongoose.service";
+import saveMessage from "../dtos/saveMessage.dto";
+import debug from "debug";
+
+const log:debug.IDebugger = debug('app:message-dao');
+
+class MessageDao{
+       Schema = mongooseService.getMongoose().Schema;
+       messageSchema = new this.Schema({
+              fullName:{
+                     type:String,
+                     required:true,
+                     validate:(fullName:string)=>(
+                            fullName.length<25
+                     )
+              },
+              email:{
+                     type:String,
+                     required:true,
+                     validate:(email:string)=>(
+                            (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(email)
+                     )
+              },
+              message:{
+                     type:String,
+                     required:true,
+                     validate:(message:string)=>(
+                            message.length>5
+                     )
+              }
+       });
+       Message = mongooseService.getMongoose().model('Message',this.messageSchema);
+       constructor(){
+              log("message dao created!");
+       }
+       async saveMessage(message:saveMessage):Promise<MessageDao>{
+              const messageObj = new this.Message({
+                     ...message
+              });
+              await messageObj.save();
+              return messageObj;
+       }
+}
+
+export default new MessageDao();
---
+++ server/message/dtos/saveMessage.dto.ts
@@ -0,0 +1,5 @@
+export default interface saveMessage{
+       fullName:string,
+       email:string,
+       message:string
+}
