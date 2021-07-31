2d653dc165663aa763ad4066c8a769a397cc3744
Joseph Benoy <josephbenoy03@gmail.com>
2021-08-01 01:24:30 +0530

    added express middleware

--- server/app.ts
+++ server/app.ts
@@ -25,6 +25,7 @@ const log:debug.IDebugger = debug('app');
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const loggerOptions:expressWinston.LoggerOptions = {
        transports:[new winston.transports.Console()],
