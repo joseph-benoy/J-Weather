a4d7a9a1707deb10748ea5dc6e74f90ca3bbd20e
Joseph Benoy <josephbenoy03@gmail.com>
2021-08-01 01:07:09 +0530

    changes to message router

--- server/message/message.routes.config.ts
+++ server/message/message.routes.config.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 import debug from 'debug';
 import CommonRoutesConfig from '../common/common.routes.config';
+import messageController from './controller/message.controller';
+
 
 const log:debug.IDebugger = debug("app:message");
 
-class MessageRoutesConfig extends 
+class MessageRoutesConfig extends CommonRoutesConfig{
+       constructor(app:express.Application){
+              super(app,'Message');
+       }
+       configureRoutes():express.Application{
+              this.app.routes("/message/save")
+              .post(messageController.saveMessage);
+              return this.app;
+       }
+}
+
+export default MessageRoutesConfig;
