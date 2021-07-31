4a318bc4f35e31d281743cd5911f01288a63b1c8
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-31 18:43:58 +0530

    added weather controller

---
+++ server/weather/controllers/weather.controller.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import debug from 'debug';
+
+const log:debug.IDebugger = debug('app:weather-controller');
+
+class WeatherController{
+       async getWeatherByCity(req:express.Request,res:express.Response,next:express.NextFunction){
+       
+       }
+}
