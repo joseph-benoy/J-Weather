fad0c40172a5fabf5bf16405b71ee509d576ca4c
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-31 18:38:29 +0530

    added weather routes config

---
+++ server/weather/weather.routes.config.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import CommonRoutesConfig from '../common/common.routes.config';
+
+export default class WeatherRoutesConfig extends CommonRoutesConfig{
+       constructor(app:express.Application){
+              super(app,'weather');
+       }
+       configureRoutes():express.Application{
+              this.app.route('/weather')
+              .get();
+       }
+};
