983415f4a071a6bc37bc230f1a496353ca982de5
Joseph Benoy <josephbenoy03@gmail.com>
2021-08-01 00:29:15 +0530

    added message dao

---
+++ server/message/daos/message.dao.ts
@@ -0,0 +1,1 @@
+import mongooseService from "../../common/services/mongoose.service";
---
+++ server/message/message.routes.config.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+import debug from 'debug';
+
+const log:debug.IDebugger = debug("app:message");
+
