581525c76cc1150a862ecdb457e7e4595077885a
Joseph Benoy <josephbenoy03@gmail.com>
2021-08-13 21:15:18 +0530

    Delete server/build directory

--- server/build/common/types/error.express.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class expressError {
-    constructor(status, message) {
-        this.status = status;
-        this.message = message;
-    }
-}
-exports.default = expressError;
